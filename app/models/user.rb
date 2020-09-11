fc98b9156e0f93eef4b5cf8284c505cadb02f665
masaya2311 <kanitama0729@icloud.com>
2020-09-11 17:45:22 +0900

    単体テストコード導入、実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,10 @@ class User < ApplicationRecord
         
   with_options presence: true do
       validates :nickname
-      validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ ,message: "is Full-width characters"}
-      validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ ,message: "is Full-width characters"}
-      validates :family_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]/ ,message: "is Full-width characters"}
-      validates :first_name_kana, format: { with: /\A[ぁ-んァ-ン一-龥]/ ,message: "is Full-width characters"}
+      validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is Full-width characters" }
+      validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: 'is Full-width characters' }
+      validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'is Full-width characters' } 
+      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'is Full-width characters' }
       validates :birthday
   end
        
---
+++ spec/factories/user.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"abe"}
+    email                 {Faker::Internet.free_email}
+    password              {"a00000"}
+    password_confirmation { password }
+    family_name           {"阿部"}
+    first_name            {"太郎"}
+    family_name_kana      {"アベ"}
+    first_name_kana       {"タロウ"}
+    birthday              {"2000-02-03"}
+  end
+end
