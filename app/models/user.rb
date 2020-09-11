3f23b32a00f96ee25d3b293976f48aaf9f50d813
masaya2311 <kanitama0729@icloud.com>
2020-09-11 19:02:11 +0900

    正規表現を変数に代入、コメントアウト削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,23 @@ class User < ApplicationRecord
         
   with_options presence: true do
       validates :nickname
-      validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is Full-width characters" }
-      validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ , message: 'is Full-width characters' }
-      validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'is Full-width characters' } 
-      validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ , message: 'is Full-width characters' }
+      
+      VALID_FAMILY_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+      validates :family_name, format: { with: VALID_FAMILY_NAME_REGEX, message: "is Full-width characters" }
+
+      VALID_FIRST_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+      validates :first_name, format: { with: VALID_FIRST_NAME_REGEX , message: 'is Full-width characters' }
+
+      VALID_FAMILY_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+      validates :family_name_kana, format: { with: VALID_FAMILY_NAME_KANA_REGEX , message: 'is Full-width characters' } 
+
+      VALID_FIRST_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+      validates :first_name_kana, format: { with: VALID_FIRST_NAME_KANA_REGEX , message: 'is Full-width characters' }
+
       validates :birthday
+
       validates :password length: { minimum: 6 }
+
       VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
       validates :email, length: { maximum: 255 },
                         format: { with: VALID_EMAIL_REGEX }
