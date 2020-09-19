7834766e206e0aaea2285526181d96c3856dc9f6
masaya2311 <kanitama0729@icloud.com>
2020-09-19 15:10:15 +0900

    エラー表示、エラーハンドリング、同じ変数を一つにする修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -19,18 +20,23 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    redirect_to root_path
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end  
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+  
 
   private
 
