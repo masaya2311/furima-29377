e342853a4aa1f9a36a93eb492a3b425ce2d3188d
masaya2311 <kanitama0729@icloud.com>
2020-09-28 14:35:18 +0900

    出品者だけが編集ページに遷移できる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -42,5 +43,11 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :postage_id, :area_id, :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    if user_signed_in? && current_user.id != @item.user_id
+       redirect_to root_path
+    end
   end
 end
